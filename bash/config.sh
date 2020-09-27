99aac11de2a1fde956e38910c4b96469635c7859
xitara <info@xitara.net>
2020-09-27 21:19:52 +0200

    add models to package folders

--- bash/config.sh
+++ bash/config.sh
@@ -2,7 +2,7 @@
 
 # list of files and folder to pack/deploy
 # STORAGE=(*.php assets classes controllers lang partials updates vendor config content layouts meta pages partials theme.yaml plugin.yaml version.yaml)
-STORAGE=(*.php assets classes controllers tests lang partials updates vendor config content layouts meta pages partials backend theme.yaml plugin.yaml version.yaml phpunit.xml)
+STORAGE=(*.php assets classes models controllers tests lang partials updates vendor config content layouts meta pages partials backend theme.yaml plugin.yaml version.yaml phpunit.xml)
 
 # filename. default name from package.json example with jq (linux)
 FILE=$(cat package.json | jq -r .name)
